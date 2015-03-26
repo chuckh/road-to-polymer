0d9bbd11ecda3caa3aa7e9a61442bb7e7d99e5a3
Chuck Horton <chuckh@hware.com>
2015-03-26 09:23:28 -0700

    Added Polymer Discussion

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,20 @@
 (function (document) {
   'use strict';
 
-
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
   app.appName = 'Road to Polymer';
   app.page = 0;
+  app.forumEmbedSrc = 'https://groups.google.com/forum/embed/?place=forum/polymer-dev'
+  + '&showsearch=true&showpopout=true&showtabs=false'
+  + '&parenturl=' + 'https://github.com/chuckh/road-to-polymer'; //encodeURIComponent(window.location.href)
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
-    console.log('Road to Polymer 1.0 is ready to rock!');
+    console.log('Road to Polymer 1.0 is ready to rock!', app.forumEmbedSrc);
   });
 
 // wrap document so it plays nice with other libraries
