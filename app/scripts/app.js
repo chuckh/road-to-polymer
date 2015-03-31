6e2ac31e1c3718ee5604656ef2eeb0c1ad1cc547
Chuck Horton <chuckh@hware.com>
2015-03-30 19:59:50 -0700

    Revert "Made Road to Polymer an App"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,18 @@
 (function (document) {
   'use strict';
 
+
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
   app.appName = 'Road to Polymer';
   app.page = 0;
-  app.forumEmbedSrc = 'https://groups.google.com/forum/embed/?place=forum/polymer-dev'
-  + '&showsearch=true&showpopout=true&showtabs=false'
-  + '&parenturl=' + 'https://github.com/chuckh/road-to-polymer'; //encodeURIComponent(window.location.href)
-
-  app.appPrefix = "" // '' for local grunt serve,  '../' for local python server, '../app/' github pages
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
-    console.log('Road to Polymer 1.0 is ready to rock!', app.forumEmbedSrc);
+    console.log('Road to Polymer 1.0 is ready to rock!');
   });
 
 // wrap document so it plays nice with other libraries
