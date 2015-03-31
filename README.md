0f6bc96cb97aaa194f2c8dee958ee5b834d10bc7
Chuck Horton <chuckh@hware.com>
2015-03-30 20:56:55 -0700

    Built road-to-polymer from commit 5cd62e7 on branch app

--- README.md
+++
@@ -1,4 +0,0 @@
-# road-to-polymer
-Road to Polymer 1.0
-
-## From Polymer 0.5 to 0.8
--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,1 @@
-(function (document) {
-  'use strict';
-
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-  app.appName = 'Road to Polymer!';
-
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
-  });
-
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-})(wrap(document));
+!function(a){"use strict";var b=a.querySelector("#app");b.appName="Road to Polymer",b.page=0,b.forumEmbedSrc="https://groups.google.com/forum/embed/?place=forum/polymer-dev&showsearch=true&showpopout=true&showtabs=false&parenturl=https://github.com/chuckh/road-to-polymer",b.appPrefix="",b.addEventListener("template-bound",function(){console.log("Road to Polymer 1.0 is ready to rock!",b.forumEmbedSrc)})}(wrap(document));
